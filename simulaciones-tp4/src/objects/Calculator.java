942d62665e44415ebe88a9e229ea5d076ddbde56
Gabriel Neil <gabriel.neil@mercadolibre.com>
2017-04-30 20:36:27 -0300

    making table

--- simulaciones-tp4/src/objects/Calculator.java
+++ simulaciones-tp4/src/objects/Calculator.java
@@ -5,6 +5,8 @@
  */
 package objects;
 
+import java.util.Random;
+
 /**
  *
  * @author gabrielneil
@@ -12,18 +14,27 @@ package objects;
 public class Calculator {
 
     public Calculator() {
-        
+
     }
 
     public void tablas(int cantSim, int desde, int hasta) {
-    
+
         for (int i = 0; i < cantSim; i++) {
             simulacion();
         }
     }
-    
-    public void simulacion(){
-        Object [][] tabla = new Object[10][9];
-        
+
+    public void simulacion() {
+        Object[][] tabla = new Object[9][10];
+        Random r = new Random();
+        int contador = 0;
+        for (int i = 0; i < tabla.length; i++) {
+            //nro experimento
+            tabla[i][0] = contador;
+            //rnd1
+            tabla[i][1] = r.nextFloat();
+            //rnd2
+            tabla[i][3] = r.nextFloat();
+        }
     }
 }
